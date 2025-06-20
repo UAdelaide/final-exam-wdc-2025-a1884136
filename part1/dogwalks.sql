294813d58fa35ff1a7c98f846516e9b5330e258e
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 03:38:12 +0000

    [automatic commit] 2025-06-20 03:38:12 UTC 14.203.47.66

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -63,4 +63,5 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 
 INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
-((SELECT user_id FROM Users WHERE username = 'carol123'))
+((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
+(())
