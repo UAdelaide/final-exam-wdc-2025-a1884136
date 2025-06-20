b50393da831d025863004d140e819b4e6fa6a154
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 10:07:07 +0000

    [automatic commit] 2025-06-20 10:07:07 UTC 14.203.47.66

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -35,7 +35,7 @@ router.get('/api/walkrequests/open', async (req, res) => {
 
 router.get('/api/walkers/summary', async (req, res) => {
   try {
-    const [rows]
+    const [rows] = await pool.query
   }
 })
 
