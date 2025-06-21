78c5f7420bae8aa5cb2072ece4cb5a7acf4cb534
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 01:39:24 +0000

    [automatic commit] 2025-06-21 01:39:24 UTC 203.63.75.185

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -28,7 +28,7 @@ router.get('/api/dogs', async (req, res) => {
     res.json(rows);
   } catch (err) {
     console.error(err);
-    res.status(500).json ()
+    res.status(500).json({ error: err.message });
   }
 })
 
